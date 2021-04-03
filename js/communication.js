217968c3686c9032a599d093667033300bcea72f
Ole Tjernstad <ott556@gmail.com>
2021-04-03 14:47:08 +0200

    only us employees

--- js/communication.js
+++ js/communication.js
@@ -1,6 +1,8 @@
 const loadEmployees = async () => {
     try {
-        const response = await fetch("https://randomuser.me/api/?results=12");
+        const response = await fetch(
+            "https://randomuser.me/api/?results=12&nat=us"
+        );
         if (response.ok) {
             const data = await response.json();
             return data.results;
